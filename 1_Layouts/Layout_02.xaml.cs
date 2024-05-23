85c37404f976ebf4aa5072c00d30db4505b7fe68
arshadrasheed <katherarshad@gmail.com>
2024-05-23 09:14:04 +0530

    Layout_02 Completed

---
+++ 1_Layouts/Layout_02.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _1_Layouts
+{
+    /// <summary>
+    /// Interaction logic for Layout_02.xaml
+    /// </summary>
+    public partial class Layout_02 : Window
+    {
+        public Layout_02()
+        {
+            InitializeComponent();
+        }
+    }
+}
